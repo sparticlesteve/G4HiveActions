c67ab17435498a2ab54d49f0b12e39362c8e92dd
Steve Farrell <sfarrell@cern.ch>
2015-07-08 11:41:58 +0200

    added step action tool base class

---
+++ G4HiveActions/SteppingActionBaseTool.h
@@ -0,0 +1,43 @@
+#ifndef G4HIVEACTIONS_STEPPINGACTIONBASETOOL_H
+#define G4HIVEACTIONS_STEPPINGACTIONBASETOOL_H
+
+// Framework includes
+#include "AthenaBaseComps/AthAlgTool.h"
+
+// Local includes
+#include "G4HiveActions/ISteppingActionTool.h"
+#include "G4HiveActions/ThreadActionHolder.h"
+
+namespace g4hive
+{
+
+  /// @class SteppingActionBaseTool
+  /// @brief Base class for stepping action tools.
+  ///
+  /// @author Steve Farrell <Steven.Farrell@cern.ch>
+  ///
+  class SteppingActionBaseTool : public AthAlgTool,
+                                 public virtual ISteppingActionTool
+  {
+
+    public:
+
+      /// Constructor
+      SteppingActionBaseTool(const std::string& type, const std::string& name,
+                             const IInterface *parent);
+
+      /// @brief Return the action for the current thread.
+      /// This method will trigger creation of the action when first
+      /// called in a new thread.
+      ISteppingAction* getAction() override final;
+
+    private:
+
+      /// Thread-local storage of my actions
+      ThreadActionHolder<ISteppingAction> m_action;
+
+  }; // class SteppingActionBaseTool
+
+} // namespace g4hive
+
+#endif
---
+++ src/SteppingActionBaseTool.cxx
@@ -0,0 +1,30 @@
+// Local includes
+#include "G4HiveActions/SteppingActionBaseTool.h"
+
+namespace g4hive
+{
+
+  //---------------------------------------------------------------------------
+  // Constructor
+  //---------------------------------------------------------------------------
+  SteppingActionBaseTool::SteppingActionBaseTool(const std::string& type,
+                                                 const std::string& name,
+                                                 const IInterface* parent)
+    : AthAlgTool(type, name, parent)
+  {
+  }
+
+  //---------------------------------------------------------------------------
+  // Return the action for the current thread
+  //---------------------------------------------------------------------------
+  ISteppingAction* SteppingActionBaseTool::getAction()
+  {
+    ISteppingAction* action = m_action.get();
+    if(!action){
+      action = makeSteppingAction();
+      m_action.set(action);
+    }
+    return action;
+  }
+
+} // namespace g4hive
