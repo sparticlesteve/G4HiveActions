681ff9706bdb7b665d07c39fcfa2e681bad6ea56
Steve Farrell <sfarrell@cern.ch>
2015-07-14 22:05:51 +0200

    adding tracking action tool interfaces

---
+++ G4HiveActions/IPostTrackingActionTool.h
@@ -0,0 +1,42 @@
+#ifndef G4HIVEACTIONS_IPOSTTRACKINGACTIONTOOL_H
+#define G4HIVEACTIONS_IPOSTTRACKINGACTIONTOOL_H
+
+// Framework includes
+#include "GaudiKernel/IAlgTool.h"
+
+// Local includes
+#include "G4HiveActions/IPostTrackingAction.h"
+
+namespace g4hive
+{
+
+  /// @class IPostTrackingActionTool
+  /// @brief Abstract interface for tools that manage ATLAS post-tracking
+  /// actions.
+  ///
+  /// @author Steve Farrell <Steven.Farrell@cern.ch>
+  ///
+  class IPostTrackingActionTool : virtual public IAlgTool
+  {
+
+    public:
+
+      /// Return the action for current thread.
+      virtual IPostTrackingAction* getAction() = 0;
+
+      /// Interface declaration
+      static const InterfaceID& interfaceID() {
+        static const InterfaceID iid_IPostTrkTool("g4hive::IPostTrackingActionTool", 1, 0);
+        return iid_IPostTrkTool;
+      }
+
+    protected:
+
+      /// Create an action for this thread. Must be implemented by tool.
+      virtual IPostTrackingAction* makePostTrackingAction() = 0;
+
+  }; // class IPostTrackingActionTool
+
+} // namespace g4hive
+
+#endif
---
+++ G4HiveActions/IPreTrackingActionTool.h
@@ -0,0 +1,42 @@
+#ifndef G4HIVEACTIONS_IPRETRACKINGACTIONTOOL_H
+#define G4HIVEACTIONS_IPRETRACKINGACTIONTOOL_H
+
+// Framework includes
+#include "GaudiKernel/IAlgTool.h"
+
+// Local includes
+#include "G4HiveActions/IPreTrackingAction.h"
+
+namespace g4hive
+{
+
+  /// @class IPreTrackingActionTool
+  /// @brief Abstract interface for tools that manage ATLAS pre-tracking
+  /// actions.
+  ///
+  /// @author Steve Farrell <Steven.Farrell@cern.ch>
+  ///
+  class IPreTrackingActionTool : virtual public IAlgTool
+  {
+
+    public:
+
+      /// Return the action for current thread.
+      virtual IPreTrackingAction* getAction() = 0;
+
+      /// Interface declaration
+      static const InterfaceID& interfaceID() {
+        static const InterfaceID iid_IPreTrkTool("g4hive::IPreTrackingActionTool", 1, 0);
+        return iid_IPreTrkTool;
+      }
+
+    protected:
+
+      /// Create an action for this thread. Must be implemented by tool.
+      virtual IPreTrackingAction* makePreTrackingAction() = 0;
+
+  }; // class IPreTrackingActionTool
+
+} // namespace g4hive
+
+#endif
