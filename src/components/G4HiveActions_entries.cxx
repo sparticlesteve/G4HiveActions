67b61c3d0c2ba95ff814fc40dc2adea2a7c16cb4
Steve Farrell <sfarrell@cern.ch>
2015-07-15 03:09:05 +0200

    updating framework entries

--- src/components/G4HiveActions_entries.cxx
+++ src/components/G4HiveActions_entries.cxx
@@ -1,11 +1,15 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 #include "../UserActionSvc.h"
+#include "../ExampleSteppingActionTool.h"
+#include "../ExampleMultiActionTool.h"
 
 DECLARE_SERVICE_FACTORY( g4hive::UserActionSvc )
-// DECLARE_TOOL_FACTORY( g4hive::SteppingActionTool )
+DECLARE_TOOL_FACTORY( g4hive::ExampleSteppingActionTool )
+DECLARE_TOOL_FACTORY( g4hive::ExampleMultiActionTool )
 
 DECLARE_FACTORY_ENTRIES( G4HiveActions ) {
   DECLARE_SERVICE( g4hive::UserActionSvc )
-  //DECLARE_TOOL( g4hive::SteppingActionTool )
+  DECLARE_TOOL( g4hive::ExampleSteppingActionTool )
+  DECLARE_TOOL( g4hive::ExampleMultiActionTool )
 }
