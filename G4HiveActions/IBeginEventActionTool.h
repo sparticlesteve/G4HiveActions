56a8c2f412ced7b854e23b2c9c1d5f081f716395
Steve Farrell <sfarrell@cern.ch>
2015-07-15 18:34:35 +0200

    fix compilation

--- G4HiveActions/IBeginEventActionTool.h
+++ G4HiveActions/IBeginEventActionTool.h
@@ -22,7 +22,7 @@ namespace g4hive
     public:
 
       /// Return the action for current thread.
-      virtual IBeginEventAction* getBeginEventAction() = 0
+      virtual IBeginEventAction* getBeginEventAction() = 0;
 
       /// Interface declaration
       static const InterfaceID& interfaceID() {
