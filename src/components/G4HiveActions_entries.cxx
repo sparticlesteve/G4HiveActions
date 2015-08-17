21f1edfe43d39e975719a2db74fee88e846f09c0
Steve Farrell <sfarrell@cern.ch>
2015-08-17 07:56:01 +0200

    Added stacking tool to entries

--- src/components/G4HiveActions_entries.cxx
+++ src/components/G4HiveActions_entries.cxx
@@ -1,15 +1,19 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 #include "../UserActionSvc.h"
+#include "../AtlasStackingActionTool.h"
 #include "../ExampleSteppingActionTool.h"
 #include "../ExampleMultiActionTool.h"
 
+
 DECLARE_SERVICE_FACTORY( g4hive::UserActionSvc )
+DECLARE_TOOL_FACTORY( g4hive::AtlasStackingActionTool )
 DECLARE_TOOL_FACTORY( g4hive::ExampleSteppingActionTool )
 DECLARE_TOOL_FACTORY( g4hive::ExampleMultiActionTool )
 
 DECLARE_FACTORY_ENTRIES( G4HiveActions ) {
   DECLARE_SERVICE( g4hive::UserActionSvc )
+  DECLARE_TOOL( g4hive::AtlasStackingActionTool )
   DECLARE_TOOL( g4hive::ExampleSteppingActionTool )
   DECLARE_TOOL( g4hive::ExampleMultiActionTool )
 }
